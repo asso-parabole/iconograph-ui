1eee246241815d70de1b1b4d3653604eb80b22a4
NathanCHEVALIER <nathanchevalier.lechatelet@gmail.com>
2025-08-17 18:21:05 +0200

    Add Lateral Menu component

---
+++ lib/navigation/MainMenu.svelte
@@ -0,0 +1,85 @@
+<script>
+    // @ts-nocheck
+
+    import MenuItem from './MenuItem.svelte';
+
+    export let isOpen = false;
+    export let menu;
+
+</script>
+
+<nav class="{isOpen ? '' : 'hidden'}">
+    <div class="logo" >
+        <!-- Logo -->
+    </div>
+
+    <div class="main">
+        <ul>
+            {#each menu.main as item}
+                <MenuItem item={item}></MenuItem>
+            {/each}
+        </ul>
+    </div>
+
+    <div class="bottom" >
+        <div>
+            <a aria-label="Signout Button" class="menu-item signout" href="/auth/signout" data-sveltekit-preload-data="off"></a>
+        </div>
+    </div>
+</nav>
+
+<style>
+    nav {
+        background-color: var(--main-bg-color);
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 85px;
+        min-height: 100vh;
+        max-height: 100vh;
+        box-sizing: border-box;
+        border-right: 1px solid var(--main-border-color);
+        padding-top: 58px;
+        padding-bottom: 10px;
+        transition: all ease-in-out 0.4s;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        z-index: 100;
+    }
+    nav.hidden {
+        transform: translateX(0px);
+    }
+    nav > .logo {
+        width: 40px;
+        height: 40px;
+        min-height: 40px;
+        background-image: var(--logo-minimized);
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center center;
+        margin-bottom: 32px;
+    }
+
+    nav > .main {
+        flex: 1;
+        width: 100%;
+        max-height: calc(100vh - 140px - 96px);
+        overflow-y: scroll;
+    }
+    nav ul {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    }
+
+    nav > .bottom {
+        background-color: aqua;
+        width: 100%
+    }
+    nav > .bottom > div {
+        height: 96px;
+        width: 30px;
+    }
+
+</style>
---
+++ lib/navigation/MenuItem.svelte
@@ -0,0 +1,64 @@
+<script>
+    export let item;
+    let isHover = false;
+</script>
+
+<li>
+    <a aria-label="{item.name} tab menu" href="{item.uri}" class="menu-item {item.selected ? 'selected' : ''}"
+        on:mouseenter={() => isHover = true} on:mouseleave={() => isHover = false} data-sveltekit-preload-data="off">
+        <div>
+            <div><!-- Lateral Color Border --></div>
+            <div style="background-image: url({item.selected || isHover ? item.iconHover: item.icon})"></div>
+        </div>
+    </a>
+</li>
+
+<style>
+    li {
+        list-style: none;
+        width: 100%;
+        margin-bottom: 14px;
+    }
+
+    .menu-item {
+        display: inline-block;
+        width: 100%;
+        height: 48px;
+        padding: 0px;
+        transition: all ease-in-out 0.17s;
+    }
+    .menu-item:hover, .menu-item.selected {
+        color: #efefef;
+        border-left-color: var(--theme-main-color);
+    }
+    .menu-item > div {
+        height: 100%;
+        display: flex;
+    }
+
+    .menu-item > div > div:nth-of-type(1) {
+        height: 100%;
+        width: 5px;
+        background-color: transparent;
+        transition: all ease-in-out 0.17s;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
+    .menu-item > div > div:nth-of-type(2) {
+        height: 48px;
+        width: 48px;
+        margin: 0px 12px;
+        border-radius: 12px;
+        background-size: 26px auto;
+        background-position: center center;
+        background-repeat: no-repeat;
+        transition: all ease-in-out 0.17s;
+    }
+
+    .menu-item.selected > div > div:nth-of-type(1) {
+        background-color: var(--theme-main-color);
+    }
+    .menu-item:hover > div > div:nth-of-type(2) {
+        background-color: var(--theme-bg-color-light);
+    }
+</style>
