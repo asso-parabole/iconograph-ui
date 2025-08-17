795d64884a2db98308165cf61a99c0bfb57ef779
NathanCHEVALIER <nathanchevalier.lechatelet@gmail.com>
2025-08-17 21:40:41 +0200

    Fix imports

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-import Button from "./lib/Button.svelte";
+import Button from "./lib/form/Button.svelte";
+import Form from "./lib/form/Form.svelte"
+import Input from "./lib/form/Input.svelte"
+import BodySection from "./lib/layout/BodySection.svelte"
+import HeadSection from "./lib/layout/HeadSection.svelte"
+import Card from "./lib/layout/Card.svelte"
+import MainMenu from "./lib/navigation/MainMenu.svelte"
+import Notification from "./lib/notification/Notification.svelte";
+import NotificationWrapper from "./lib/notification/NotificationWrapper.svelte";
 
-export { Button };
+export {
+    Button,
+    Form,
+    Input,
+    BodySection,
+    HeadSection,
+    Card,
+    MainMenu,
+    Notification,
+    NotificationWrapper
+};
